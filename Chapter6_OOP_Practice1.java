1b9e1d030c95185b65d6a8c5f587bb6cbc5534d8
raccoon-ccoder <qorwjddus96@naver.com>
2021-02-13 23:28:50 +0900

    Create Chapter6_OOP_Practice1.java

---
+++ Chapter6_OOP_Practice1.java
@@ -0,0 +1,11 @@
+// 두 점의 거리를 계산사는 getDistance()를 완성하시오.
+public class Chapter6_OOP_Practice1 {
+
+	public static void main(String[] args) {
+		System.out.println(getDistance(1,1,2,2));
+	}
+	static double getDistance(int x, int y, int x1, int y1) {
+		return Math.sqrt((x1 - x)*(x1 - x) + (y1 - y)*(y1 - y));
+	}
+	
+}
